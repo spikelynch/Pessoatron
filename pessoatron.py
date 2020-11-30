cec05bc6e5f0ffb3f58d553f9a4494cb33ca6166
Mike Lynch <Michael.Lynch@uts.edu.au>
2020-11-30 11:07:10 +1100

    Refactored pessoatron.py so that it builds the permutations as a data structure and feeds that to a pystache template to write the output

--- pessoatron.py
+++ pessoatron.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-import sys
+import sys, os
 from itertools import permutations
 from collections import Counter
 import pystache
-
+import argparse
 
 # The synthemes were compiled by hand and the synthematic totals were taken
 # from Greg Egan's diagram of the Tutt-Coxeter graph on this page by John
@@ -41,8 +41,26 @@ PERM6 = list(permutations([1, 2, 3, 4, 5, 6]))
 
 PERM6D = {}
 
+STRUCTURE_TEMPLATE = 'structure.html'
+
 for i in range(1, len(PERM6) + 1):
 	PERM6D[i] = f'{PERM6[i - 1]}'
+
+
+def load_template(d, f):
+	template = os.path.join(d, f)
+	with open(template, 'r') as fh:
+		contents = fh.readlines()
+		return '\n'.join(contents)
+	sys.process.exit(-1)
+
+def write_file(d, f, contents):
+	filepath = os.path.join(d, f)
+	with open(filepath, 'w') as fh:
+		fh.write(contents)
+
+
+
 
 def nspan(i):
 	return f'<span class="s{i}">{i}</span>'
@@ -73,57 +91,68 @@ def lookup_perm6(p):
 	sys.stderr.write(f"mismatched perm {p}\n")
 	return "-"
 
-print("""
-<html>
-<head>
-<link href="combos.css" rel="stylesheet" />
-</head>
-<body>
-""")
 
-sys.stderr.write("Permuting...\n")
+def make_permutations():
+	i = 0
+	find_dups = {}
+	results = []
+	for p in PERM6:
+		perm = {}
+		#print(f'<h3>{i}: {p}</h3>')
+		i += 1
+		perm['i'] = i
+		perm['p'] = f'{p}'
+		perm['signature'] = ''
+		perm['totals'] = []
+		for tname, t in TOTALS.items():
+			#print('<div class="row">' + tname + ": ")
+			total = { 'name': tname, 'synthemes': [] }
+			automorphism = []
+			for s in t:
+				rev = []
+				syntheme = { 'duads': [] }
+				for x, y in SYNTHEMES[s]:
+					x0 = p[x - 1]
+					y0 = p[y - 1]
+					#syntheme['duads'] += '(' + nspan(x0) + ' ' + nspan(y0) + ')'
+					syntheme['duads'].append((x0, y0))
+					if x0 > y0:
+						z0 = x0
+						x0 = y0
+						y0 = z0
+					rev.append(( x0, y0 ))
+				total['synthemes'].append(syntheme)
+				syntheme['name'] = lookup_syntheme(rev)
+				automorphism.append(syntheme['name'])
+				perm['signature'] += syntheme['name']
+			autot = lookup_synthematic_total(automorphism)
+			total['automorphism'] = autot
+			perm['totals'].append(total)
+			# print(f"--&gt;{autot}")
+			# print("</div>")
+		if perm['signature'] in find_dups:
+			find_dups[perm['signature']] += 1
+			sys.stderr.write("Duplicate " + perm['signature'] + "\n")
+		else:
+			find_dups[perm['signature']] = 1
+			#print(f"<div>{seq}</div>")
+		results.append(perm)
+	return results
+
+def render_structure(d, perms):
+	t = load_template(d, STRUCTURE_TEMPLATE)
+	return pystache.render(t, { 'permutations': perms })
 
 
-i = 1
+ap = argparse.ArgumentParser()
 
-find_dups = {}
+ap.add_argument('-d', '--data', default ="./data", help="Directory containing templates and word lists")
+ap.add_argument('-o', '--output', default="./output", help="Directory to write output")
 
-for p in PERM6:
-	print(f'<h3>{i}: {p}</h3>')
-	i += 1
-	seq = ''
-	for tname, t in TOTALS.items():
-		print('<div class="row">' + tname + ": ")
-		automorphism = []
-		for s in t:
-			rev = []
-			ss = ''
-			for x, y in SYNTHEMES[s]:
-				x0 = p[x - 1]
-				y0 = p[y - 1]
-				ss = ss + '(' + nspan(x0) + ' ' + nspan(y0) + ')'
-				if x0 > y0:
-					z0 = x0
-					x0 = y0
-					y0 = z0
-				rev.append(( x0, y0 ))
-			rs = lookup_syntheme(rev)
-			print(rs + ' ')
-			automorphism.append(rs)
-			seq += rs
-		autot = lookup_synthematic_total(automorphism)
-		print(f"--&gt;{autot}")
-		print("</div>")
-	if seq in find_dups:
-		find_dups[seq] += 1
-		sys.stderr.write("Duplicate " + seq + "\n")
-	else:
-		find_dups[seq] = 1
-		print(f"<div>{seq}</div>")
+args =  ap.parse_args()
 
-print("\n")
+p = make_permutations()
 
-print("""
-</body>
-</html>
-""")
+html = render_structure(args.data, p)
+
+write_file(args.output, 'structure.html', html)
